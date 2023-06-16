b19b19e3e5fcfbbe422aefc3e024933c72fdd394
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-06-16 17:07:42 -0300

    Feat: adiciona código para salvar nome no localStorage

--- blackjack/src/pages/Home.jsx
+++ blackjack/src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ButtonRules from '../components/ButtonRules';
 import ButtonPlayGame from '../components/ButtonPlayGame';
 
 function Home() {
+  const [name, setName] = useState("");
+
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  }
+
+  useEffect(() => {
+    // Armazena o nome no armazenamento local
+    localStorage.setItem('name', name);
+  }, [name]);
+
   return (
     <main>
     <div>
@@ -18,6 +29,8 @@ function Home() {
         type="text"
         id="name"
         name="name"
+        value={ name }
+        onChange={ handleChange }
       />
       <ButtonPlayGame />
       <ButtonRules />
