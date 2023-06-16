0851db53b5872ea27a729a232d39d29773a6f3fa
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-06-16 17:07:02 -0300

    Feat: adiciona código para pegar nome do localStorage

--- blackjack/src/pages/Game.jsx
+++ blackjack/src/pages/Game.jsx
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
 function Game() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    // Recupera o nome do armazenamento local, se existir
+    const storedName = localStorage.getItem('name');
+    if (storedName) {
+      setName(name);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Bem vindo ao jogo</h1>
+      <h1>Bem vindo ao jogo, ${name}</h1>
     </div>
   )
 }
