7dc509e564155354e0c51fffa2ab590888869c08
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-06-16 18:11:14 -0300

    Fix: altera código por conta da versão do react-router-dom

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Rules from './pages/Rules';
 import Game from './pages/Game';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" render={ () => <Home /> } />
-      <Route exact path="/rules" render={ () => <Rules /> } />
-      <Route exact path="/game" render={ () => <Game /> } />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route exact path="/rules" element={ <Rules /> } />
+        <Route exact path="/game" element={ <Game /> } />
+      </Routes>
+    </Router>
   );
 }
 
